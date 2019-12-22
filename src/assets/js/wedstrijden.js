f5c231013126d2b684d6da8f1d8f09695c520d7f
JoshuaDaoud <joshua.hully@gmail.com>
2019-12-22 09:13:53 +0100

    Wedstrijdlist is populated from database

--- src/assets/js/wedstrijden.js
+++ src/assets/js/wedstrijden.js
@@ -1,14 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-
 function getLastTenGames() {
-  let tableBodyEl = document.querySelector('#firstUpcomingGames tbody')
   var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function() { 
           if (this.readyState == 4) {
               console.log(this.responseText);
               let wedstrijden = JSON.parse(this.responseText);
               for (let index = 0; index < wedstrijden.length; index++) {
-                tableBodyEl.append(createTableRow(index, wedstrijden[index]));
+                  createTableRow(index, wedstrijden[index]);
               }
               console.log(wedstrijden);
           }
@@ -21,24 +18,9 @@ function createTableRow(index, wedstrijd) {
   let row = document.createElement('tr');
   row.innerHTML =     '<tr>' + 
                           '<th scope="row">' + index + '</th>' +
-                          '<td>' + wedstrijd.thuisTeam.naamTeam + ' - ' + wedstrijd.uitTeam.naamTeam + '</td>' +
+                          '<td>' + wedstrijd.teamThuis + ' - ' + wedstrijd.teamUit + '</td>' +
                           '<td>' + wedstrijd.competitie + '</td>' +
                           '<td>' + wedstrijd.beginTijd + '</td>' +
                       '</tr>';
   return row;
-}
-
-@Component({
-  selector: 'app-wedstrijd-list',
-  templateUrl: './wedstrijd-list.component.html',
-  styleUrls: ['./wedstrijd-list.component.css']
-})
-export class WedstrijdListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-    getLastTenGames();
-  }
-
 }
